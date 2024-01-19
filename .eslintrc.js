a323c428db2f6990bf39e877558d6c8ad989330f
Maksym Svoboda <maksim4ik71@gmail.com>
2024-01-19 18:57:41 +0200

    backend environment setup finished

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  ignorePatterns: [".eslintrc.js"],
+  extends: "@mate-academy/eslint-config",
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "max-len": [
+      "error",
+      {
+        ignoreTemplateLiterals: true,
+        ignoreComments: true,
+      },
+    ],
+  },
+};
