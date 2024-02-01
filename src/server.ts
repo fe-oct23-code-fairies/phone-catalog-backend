5a4b1b2f0d5bb21c9bc5649bc945ef65a1f0b2c3
Maksym Svoboda <maksim4ik71@gmail.com>
2024-02-01 15:31:42 +0200

    implemented auth using DB

--- src/server.ts
+++ src/server.ts
@@ -9,19 +9,22 @@ import { phonesRouter } from './routes/detailedProducts/phones/phones.routes.js'
 import { tabletsRouter } from './routes/detailedProducts/tablets/tablets.routes.js';
 import { productsRouter } from './routes/products/products.routes.js';
 import { detailedProductsRouter } from './routes/detailedProducts/detailedProducts.routes.js';
+import { authRouter } from './routes/auth/auth.routes.js';
 
 export const serverInit = () => {
   const app = express();
   const dirname = path.dirname('.');
 
   app.use(express.json());
-  app.use(cors());
+  app.use(cors({ origin: process.env.ORIGIN }));
 
   app.get('/', (_, res) => {
     res.send('https://www.youtube.com/watch?v=G510jeWiaV0');
   });
 
   app.use('/static', express.static(path.join(dirname, 'public')));
+
+  app.use('/auth', authRouter);
 
   app.use('/accessories', accessoriesRouter);
   app.use('/phones', phonesRouter);
